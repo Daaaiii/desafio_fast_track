844c8c844d74cd44444575a8ceb8c3eb8f5c3775
Daiane Bolzan <dainha85@gmail.com>
2023-12-02 22:54:47 -0300

    feat: add check for inputs

--- js/todo.js
+++ js/todo.js
@@ -1,39 +1,113 @@
 const btnLogout = document.getElementById("logout");
+const btnTask = document.getElementById("task-btn");
 
 function addZero(num) {
-    return num < 10 ? "0" + num : num;
+	return num < 10 ? "0" + num : num;
 }
 
 window.onload = function () {
-    setInterval(() => {
-        const time = document.getElementById("time");
-        let hours = new Date();
-        let now =
-            addZero(hours.getHours()) +
-            ":" +
-            addZero(hours.getMinutes()) +
-            ":" +
-            addZero(hours.getSeconds());
-        time.innerText = now;
-    }, 1000);
+	setInterval(() => {
+		const time = document.getElementById("time");
+		let hours = new Date();
+		let now = addZero(hours.getHours()) + ":" + addZero(hours.getMinutes());
+		time.innerText = now;
+	}, 1000);
 
-    const day = document.getElementById("today");
-    let data = new Date();
+	const day = document.getElementById("today");
+	let data = new Date();
 
-    let today =
-        addZero(data.getDate()) +
-        "/" +
-        addZero(data.getMonth() + 1) + 
-        "/" +
-        data.getFullYear();
-    day.innerText = today;
+	let today =
+		addZero(data.getDate()) +
+		"/" +
+		addZero(data.getMonth() + 1) +
+		"/" +
+		data.getFullYear();
+	day.innerText = today;
 };
 
 const logout = () => {
-    btnLogout.addEventListener("click", function (event) {
-        event.preventDefault();
-        // localStorage.removeItem()
-        window.location.href = "index.html";
-    });
+	btnLogout.addEventListener("click", function (event) {
+		event.preventDefault();
+		window.location.href = "index.html";
+	});
 };
 logout();
+
+const checkInputs = () => {
+	const inputIds = [
+		"task",
+		"init",
+		"hourInit",
+		"end",
+		"hourEnd",
+		"description",
+	];
+
+	for (const inputId of inputIds) {
+		const inputValue = document.getElementById(inputId).value.trim();
+
+		if (inputValue === "") {
+			return false;
+		}
+	}
+	return true;
+};
+
+const createTask = () => {
+	btnTask.addEventListener("click", (event) => {
+		event.preventDefault();
+
+		const form = document.querySelector(".needs-validation");
+
+		if (!form.checkValidity()) {
+			form.classList.add("was-validated");
+			return;
+		}
+
+		const taskName = document.getElementById("task").value;
+		const initDate = document.getElementById("init").value;
+		const initTime = document.getElementById("hourInit").value;
+		const endDate = document.getElementById("end").value;
+		const endTime = document.getElementById("hourEnd").value;
+		const description = document.getElementById("description").value;
+		const taskId = new Date().getTime().toString();
+		let existingTasks = [];
+		const existingTasksJSON = localStorage.getItem("taskData");
+
+		if (existingTasksJSON) {
+			try {
+				existingTasks = JSON.parse(existingTasksJSON);
+			} catch (error) {
+				console.error("Erro ao analisar tarefas existentes:", error);
+			}
+		}
+		const task = {
+			id: taskId,
+			taskName: taskName,
+			initDate: initDate,
+			initTime: initTime,
+			endDate: endDate,
+			endTime: endTime,
+			description: description,
+		};
+
+		const taskJSON = JSON.stringify(task);
+        console.log('taskjson',taskJSON)
+        console.log('existingTasks',existingTasks)
+        existingTasks.push(taskJSON);
+
+		localStorage.setItem("taskData", existingTasks);
+
+		const successAlert = document.createElement("div");
+		successAlert.className = "alert alert-success mt-3";
+		successAlert.textContent = "Tarefa criada e salva com sucesso!";
+
+		document.getElementById("alert").appendChild(successAlert);
+
+		setTimeout(() => {
+			successAlert.remove();
+		}, 2000);
+	});
+};
+
+createTask();
